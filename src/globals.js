5c61732727065a38e9cacd73f227aaee6c9dfde0
TriggerT <noah.kd@gmail.com>
2022-12-15 00:57:20 -0300

    kans

--- src/globals.js
+++ src/globals.js
@@ -1,7 +1,7 @@
 
 
 window.addEventListener( "load", ev => {
-	var links = document.querySelectorAll( "li[link]" );
+	var links = document.querySelectorAll( "*[link]" );
 
 	links.forEach( link => link.addEventListener( "click", ev => {
 		window.open( link.getAttribute( "link" ), "_self" );
